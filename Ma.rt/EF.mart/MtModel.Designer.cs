b1aecf9c41397045efbe043d4d99d92041113278
Vaibhav Kumar <vaibhavkumar5219@gmail.com>
2022-09-15 16:06:26 +0530

    office connection string added

--- Ma.rt/EF.mart/MtModel.Designer.cs
+++ Ma.rt/EF.mart/MtModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\hp\OneDrive\Documents\GitHub\Ma.rt\Ma.rt\EF.mart\MtModel.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\GitHub Repositry\New folder\ma.rt\Ma.rt\EF.mart\MtModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
